7954ec5f69da9c2f48d1fe3a426dbea680f9c99a
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-05-17 23:00:56 -0300

    Configurações de senha Identity

--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -38,6 +38,13 @@ namespace UsuariosApi
             services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
                     .AddEntityFrameworkStores<UserDbContext>();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+            });
+
             //Injeção da service
             services.AddScoped<CadastroService, CadastroService>();
 
